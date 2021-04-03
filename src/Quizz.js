c331323ed9532bc9caadd78113d7c297aec1ea07
Ammar Qureshi <qureshiaq2003@gmail.com>
2021-04-03 16:03:58 +0500

    Push

--- src/Quizz.js
+++ src/Quizz.js
@@ -1,15 +1,44 @@
 import React, { useState } from "react";
+import Image from "./Asset/Imag/imag.jpg";
 
 function Quizz() {
-    return(
-        <div>
-            <h1>
-                Quizz App
-            </h1>
-            <div style={{border:"solid black"}}>
+//   const [data, setData] = useState([]);
+//   const getDta = () => {
+//     fetch("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
+//       .then((res) => res.json())
+//       .then((data) => setData(data));
+//   };
+//   console.log("--->>", data);
+  return (
+    <div>
+      <div id="heading-div">
+        <h3>QUIZ CATEGORIES</h3>
+      </div>
+      {/* <div style={{width:"20%",border:"1px solid red",justifyContent:"center"}}>
+            </div> */}
 
-            </div>
+      <div id="card-div">
+        <div id="button-div">
+          <button className="js-button">
+            Html
+          </button>
+          {/* {data.map((item) => {
+            return (
+              <p>
+                {item.results.question}
+              </p>
+            );
+          })} */}
+          <button className="css-button">Css</button>
+          <br />
+          <button className="js-button">Javascript</button>
+          <button className="css-button">React</button>
+          <br />
+          <button className="js-button">React Native</button>
+          <button className="css-button">C++</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 export default Quizz;
